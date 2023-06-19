e805d4c3ae5916c8dbe97ebe921b3f5d1b49e954
Vladimir Bobrovskii <bobrovskiivs@gmail.com>
2023-06-19 16:44:27 +0400

    qualifier

--- SpringCore/src/main/java/PopMusic.java
+++
@@ -1,6 +0,0 @@
-public class PopMusic implements  Music{
-    @Override
-    public String getSong() {
-        return "Ooops! I did it again!";
-    }
-}
--- SpringCore/src/main/java/RockMusic.java
+++
@@ -1,6 +0,0 @@
-public class RockMusic implements Music {
-    @Override
-    public String getSong() {
-        return "Wind cries Mary";
-    }
-}
